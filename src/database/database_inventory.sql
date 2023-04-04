eb52141c346a486e1e5870f73da285b6cf05a118
Andryan <103846142+andryann007@users.noreply.github.com>
2023-04-04 10:05:20 +0700

    Update Database

--- src/database/database_inventory.sql
+++ src/database/database_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 17, 2023 at 07:22 AM
+-- Generation Time: Apr 04, 2023 at 04:59 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -96,7 +96,7 @@ CREATE TABLE `data_customer` (
 --
 
 INSERT INTO `data_customer` (`id_customer`, `nama_customer`, `alamat`, `email`, `telp`) VALUES
-('CST-01', 'Budi Gunawan', 'Jln. Medan Merdeka Barat no 2', 'budi@gmail.com', '-'),
+('CST-01', 'Budi Gunawan', 'Jln. Pembangunan 2, Pasar Permai Cikarang', '', '-'),
 ('CST-02', 'Agung', 'Jln. Medan Merdeka Barat no 2', 'agung@gmail.com', '-');
 
 -- --------------------------------------------------------
@@ -120,8 +120,28 @@ CREATE TABLE `data_stock` (
 --
 
 INSERT INTO `data_stock` (`id_barang`, `nama_barang`, `kategori`, `qty_stock`, `harga_satuan`, `total_harga`, `status`) VALUES
-('S-01', 'Beras Putih 1 Kg', 'Sembako', -200, 12000, 1200000, 'Tersedia'),
-('S-02', 'Beras Merah 1 Kg', 'Sembako', 75, 15000, 1500000, 'Tersedia');
+('B-01', 'Royco Ayam & Sapi (12 Sachet)', 'Bumbu Dapur', 50, 6500, 325000, 'Tersedia'),
+('B-02', 'Masako Ayam & Sapi (12 sachet)', 'Bumbu Dapur', 50, 5500, 275000, 'Tersedia'),
+('B-03', 'Mamasuka Kentucky (210 gr)', 'Bumbu Dapur', 20, 5000, 100000, 'Tersedia'),
+('M-01', 'Aqua Air Mineral 1 Dus (600 ml)', 'Minuman', 10, 50000, 500000, 'Tersedia'),
+('M-02', 'Le Minerale 600 ml (1 Dus)', 'Minuman', 10, 50000, 500000, 'Tersedia'),
+('M-03', 'Coca Cola 390 ml (1 Dus)', 'Minuman', 10, 65000, 650000, 'Tersedia'),
+('MI-01', 'Indomie Goreng 1 Dus (40 pcs)', 'Makanan Instan', 10, 125000, 1250000, 'Tersedia'),
+('MI-02', 'Indomie Soto Ayam 1 Dus (40 pcs)', 'Makanan Instan', 10, 125000, 1250000, 'Tersedia'),
+('MI-03', 'Indomie Kari Ayam 1 Dus (40 pcs)', 'Makanan Instan', 10, 125000, 1250000, 'Tersedia'),
+('MI-04', 'Sedaap Mie Goreng 1 Dus (40 Pcs)', 'Sembako', 10, 122000, 1220000, 'Tersedia'),
+('MI-05', 'Sedaap Mie Soto 1 Dus (40 Pcs)', 'Sembako', 10, 122000, 1220000, 'Tersedia'),
+('MR-01', 'Better Sandwich Biscuit (20 pcs)', 'Makanan Ringan', 15, 18000, 270000, 'Tersedia'),
+('MR-02', 'Beng Beng Wafer 1 Box (20 pcs)', 'Makanan Ringan', 20, 19000, 380000, 'Tersedia'),
+('MR-03', 'Roma Malkist Original (125gr)', 'Makanan Ringan', 50, 8600, 430000, 'Tersedia'),
+('MR-04', 'Oreo Original (133gr)', 'Makanan Ringan', 30, 8500, 255000, 'Tersedia'),
+('MR-05', 'Choki Choki Original (20 pcs)', 'Makanan Ringan', 15, 20000, 300000, 'Tersedia'),
+('PM-01', 'Sabun Dettol Original (100 gr)', 'Perlengkapan Mandi & Mencuci', 20, 5000, 100000, 'Tersedia'),
+('S-01', 'Beras Putih 1 Kg', 'Sembako', 100, 12000, 1200000, 'Tersedia'),
+('S-02', 'Beras Merah 1 Kg', 'Sembako', 50, 15000, 750000, 'Tersedia'),
+('S-03', 'Telur Ayam Negeri 1 Kg', 'Sembako', 50, 24000, 1200000, 'Tersedia'),
+('S-04', 'Minyak Goreng Bimoli 1 Liter', 'Sembako', 20, 22000, 440000, 'Tersedia'),
+('S-05', 'Kopi Kapal Api (165gr)', 'Sembako', 20, 15000, 300000, 'Tersedia');
 
 -- --------------------------------------------------------
 
@@ -142,8 +162,9 @@ CREATE TABLE `data_supplier` (
 --
 
 INSERT INTO `data_supplier` (`id_supplier`, `nama_supplier`, `alamat`, `email`, `telp`) VALUES
-('SPY-01', 'PT. Unilever Indonesia', 'Jl. Jatinegara Barat no.10', 'unilever@gmail.com', '-'),
-('SPY-02', 'PT. Unilever Zimbabwe', 'Jl. Jatinegara Barat no.10', 'unilever@gmail.com', '-');
+('SPY-01', 'PT. Sayap Mas Abadi', 'Jl. Pembangunan 1 no 5, Cikarang', '-', '-'),
+('SPY-02', 'PT. Cipta Naga Semesta', 'Jl. Diponegoro km38, no 09, rt 02', '-', '-'),
+('SPY-03', 'PT. Pinus Merah Abadi', 'Jl. Arief Rahman Hakim no 18', '-', '-');
 
 -- --------------------------------------------------------
 
@@ -166,7 +187,9 @@ CREATE TABLE `data_user` (
 --
 
 INSERT INTO `data_user` (`id_user`, `nama_lengkap`, `email`, `password`, `telp`, `alamat`, `tipe_akun`) VALUES
-('USR-01', 'Andryan', 'andryan@gmail.com', '12345678', '0812-xxxx-xx12', 'Jln. Hidup Baru gg L no 65', 'Owner');
+('USR-01', 'Andryan', 'andryan@gmail.com', '12345678', '0812-xxxx-xx13', 'Jln. Hidup Baru gg L no 65', 'Owner'),
+('USR-02', 'User', 'user@gmail.com', '12345678', '-', '-', 'User'),
+('USR-03', 'Admin', 'admin@gmail.com', '12345678', '-', '-', 'Admin');
 
 --
 -- Indexes for dumped tables
